336ff7d9f636f0acdfd129e26c7bd4345598c047
Anton <47394316+PigCakee@users.noreply.github.com>
2021-05-07 14:10:50 +0300

    Update train.py

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ def build_model():
   x = data_augmentation(inputs)
   x = EfficientNetB0(include_top=False, weights='imagenet', input_tensor = x)
   x.trainable = False
-  x = tf.keras.layers.GlobalAveragePooling2D()(x.output
+  x = tf.keras.layers.GlobalAveragePooling2D()(x.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
