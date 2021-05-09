43a04855a958f08ed9dc7ee06de0f6a503642535
Anton <47394316+PigCakee@users.noreply.github.com>
2021-05-09 16:37:15 +0300

    Update train.py

--- train.py
+++ train.py
@@ -102,7 +102,7 @@ def main():
 
   log_dir='{}/f101-{}'.format(LOG_DIR, time.time())
   model.compile(
-    optimizer=tf.optimizers.Adam(lr=1e-9),
+    optimizer=tf.optimizers.Adam(lr=1e-8),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
